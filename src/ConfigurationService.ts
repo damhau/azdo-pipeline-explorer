f5e5ff73aa84326aa595fffe7c99acf8e57ad1f3
Damien Hauser <dhauser@icrc.org>
2024-09-18 16:54:20 +0200

    fix typo in configurationservice

--- src/ConfigurationService.ts
+++ src/ConfigurationService.ts
@@ -140,7 +140,7 @@ export class ConfigurationService {
         });
 
         if (inputPat) {
-            await this.secretManager.storeSecret('PAT', inputPat);
+            await this.secretManager!.storeSecret('PAT', inputPat);
             vscode.window.showInformationMessage('Configuration saved successfully.');
             vscode.commands.executeCommand('workbench.action.reloadWindow');
         } else {
